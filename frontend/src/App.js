50386faeef8c3f2aca75075890699fb6a0b6bad6
Kanika676 <kanika.19jics100@jietjodhpur.ac.in>
2025-02-23 11:00:02 +0530

    HomeScreen Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 import React from 'react'
 
@@ -10,7 +11,7 @@ const App = () => {
     <Header />
     <main className="py-3">
       <Container>
-    <h1>Welcome to proshop</h1>
+    <HomeScreen></HomeScreen>
     </Container>
     </main>
     <Footer></Footer>
